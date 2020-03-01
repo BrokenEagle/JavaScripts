f17298890e2a439e71afcfbe479d7c01eaf59fc4
BrokenEagle <BrokenEagle98@yahoo.com>
2020-03-01 15:43:38 -0800

    SiteTagSearches: Complete rewrite
    - Brought the code up to par with other scripts
    - Added a settings menu
    - Updated site URLs as required
    -- Removed BCY.net as it no longer appears to have a text tag search
    - Fixed script selecting wrong text for the main tag
    -- On the post search page
    - Use program shortcut for all program selectors
    - Several other refactors

--- sitetagsearches.user.js
+++ sitetagsearches.user.js
@@ -1,173 +1,343 @@
 // ==UserScript==
 // @name         SiteTagSearches
 // @namespace    https://github.com/BrokenEagle/JavaScripts
-// @version      3.1
+// @version      4.0
+// @description  Presents additional site links for the wiki tag(s).
 // @source       https://danbooru.donmai.us/users/23799
-// @description  Presents additional site links for the wiki tag(s)
 // @author       BrokenEagle
-// @match        *://*.donmai.us/wiki_pages/*
 // @match        *://*.donmai.us/posts?*
+// @match        *://*.donmai.us/wiki_pages/*
+// @match        *://*.donmai.us/settings
+// @exclude      /^https?://\w+\.donmai\.us/.*\.(xml|json|atom)(\?|$)/
 // @grant        none
 // @run-at       document-end
 // @downloadURL  https://raw.githubusercontent.com/BrokenEagle/JavaScripts/stable/sitetagsearches.user.js
+// @require      https://raw.githubusercontent.com/BrokenEagle/JavaScripts/20191221/lib/debug.js
+// @require      https://raw.githubusercontent.com/BrokenEagle/JavaScripts/20191221/lib/load.js
+// @require      https://raw.githubusercontent.com/BrokenEagle/JavaScripts/20191221/lib/utility.js
+// @require      https://raw.githubusercontent.com/BrokenEagle/JavaScripts/20191221/lib/validate.js
+// @require      https://raw.githubusercontent.com/BrokenEagle/JavaScripts/20191221/lib/storage.js
+// @require      https://raw.githubusercontent.com/BrokenEagle/JavaScripts/20191221/lib/danbooru.js
+// @require      https://raw.githubusercontent.com/BrokenEagle/JavaScripts/20191221/lib/menu.js
 // ==/UserScript==
 
-/***Global variables***/
+/* global $ JSPLib Danbooru */
 
-const program_css = `
-    .wiki-other-name {
-        display: inline-block;
-        position: relative;
-    }
+/****Global variables****/
 
-    .wiki-other-name .image-links,
-    .wiki-other-name .booru-links {
-        position: absolute;
-        background: white;
-        border: lightgrey solid 1px;
-        display: none;
-    }
+//Exterior script variables
+const DANBOORU_TOPIC_ID = '14958';
+const JQUERY_TAB_WIDGET_URL = 'https://cdn.jsdelivr.net/gh/jquery/jquery-ui@1.12.1/ui/widgets/tabs.js';
 
-    .wiki-other-name .image-links {
-        right: 0;
-        z-index: 20;
-    }
+//Variables for load.js
+const PROGRAM_LOAD_REQUIRED_VARIABLES = ['window.jQuery'];
+const PROGRAM_LOAD_OPTIONAL_SELECTORS = ['#c-wiki-pages #a-show', '#c-posts #a-index', '#c-users #a-edit'];
 
-    .wiki-other-name .booru-links {
-        left: 0;
-        z-index: 30;
-    }
+//Program name constants
+const PROGRAM_SHORTCUT = 'sts';
+const PROGRAM_CLICK = 'click.sts';
+const PROGRAM_NAME = 'SiteTagSearches';
 
-    .wiki-other-name .image-links ul,
-    .wiki-other-name .booru-links ul    {
-        padding: 5px;
-        margin: 0;
-    }
+//Main program variable
+var STS;
 
-    .wiki-other-name .image-links li,
-    .wiki-other-name .booru-links li    {
-        list-style-type: none;
-    }
+//Available setting values
+const BOORU_SITES = ['gelbooru', 'yandere', 'sankaku', 'konachan'];
+const SOURCE_SITES = ['pixiv', 'nicoseiga', 'twitter', 'deviantart', 'tumblr', 'artstation', 'E-Hentai', 'tinami'];
 
-    .ui-icon {
-        display: inline-block;
-    }
+//Main settings
+const SETTINGS_CONFIG = {
+    booru_sites_enabled: {
+        allitems: BOORU_SITES,
+        default: BOORU_SITES,
+        validate: (data)=>{return JSPLib.menu.validateCheckboxRadio(data, 'checkbox', BOORU_SITES);},
+        hint: "Select to show booru type."
+    },
+    source_sites_enabled: {
+        allitems: SOURCE_SITES,
+        default: SOURCE_SITES,
+        validate: (data)=>{return JSPLib.menu.validateCheckboxRadio(data, 'checkbox', SOURCE_SITES);},
+        hint: "Select to show source type."
+    },
+};
 
-`;
+//CSS constants
+
+const PROGRAM_CSS = `
+.wiki-other-name {
+    display: inline-block;
+    position: relative;
+}
+.sts-links {
+    position: absolute;
+    background-color: var(--body-background-color);
+    border: var(--footer-border);
+    display: none;
+    z-index: 1;
+}
+.sts-source-links {
+    right: 0;
+}
+.sts-booru-links {
+    left: 0;
+}
+.wiki-other-name .sts-links ul {
+    padding: 5px;
+    margin: 0;
+}
+.wiki-other-name .sts-links li {
+    list-style-type: none;
+}
+.sts-main-tagtext {
+    display: inline-block;
+    min-width: 8em;
+}
+.sts-other-tagtext {
+    display: inline-block;
+    min-width: 4.25em;
+}
+#wiki-page-body,
+#excerpt .prose {
+    min-height: 15em;
+}`;
+
+//HTML constants
+
+const STS_MENU = `
+<div id="sts-script-message" class="prose">
+    <h2>${PROGRAM_NAME}</h2>
+    <p>Check the forum for the latest on information and updates (<a class="dtext-link dtext-id-link dtext-forum-topic-id-link" href="/forum_topics/${DANBOORU_TOPIC_ID}">topic #${DANBOORU_TOPIC_ID}</a>).</p>
+</div>
+<div id="sts-console" class="jsplib-console">
+    <div id="sts-settings" class="jsplib-outer-menu">
+        <div id="sts-general-settings" class="jsplib-settings-grouping">
+            <div id="sts-general-message" class="prose">
+                <h4>General settings</h4>
+            </div>
+        </div>
+        <div id="sts-source-settings" class="jsplib-settings-grouping">
+            <div id="sts-source-message" class="prose">
+                <h4>Source settings</h4>
+            </div>
+        </div>
+        <hr>
+        <div id="sts-settings-buttons" class="jsplib-settings-buttons">
+            <input type="button" id="sts-commit" value="Save">
+            <input type="button" id="sts-resetall" value="Factory Reset">
+        </div>
+    </div>
+</div>`;
+
+//Site constants
+
+const SITE_CONFIG = {
+    pixiv: {
+        url: 'https://www.pixiv.net/en/tags/%s/artworks',
+    },
+    nicoseiga: {
+        url: 'https://seiga.nicovideo.jp/tag/%s?sort=image_created',
+    },
+    nijie: {
+        url: 'https://nijie.info/search.php?word=%s',
+    },
+    tinami: {
+        url: 'https://www.tinami.com/search/list?keyword=%s',
+    },
+    deviantart: {
+        url: 'https://www.deviantart.com/tag/%s',
+    },
+    artstation: {
+        url: 'https://www.artstation.com/search?q=%s',
+    },
+    tumblr: {
+        url: 'https://www.tumblr.com/tagged/%s',
+    },
+    twitter: {
+        url: 'https://twitter.com/hashtag/%s',
+    },
+    'E-Hentai': {
+        url: 'https://e-hentai.org/?f_search=%s',
+    },
+    gelbooru: {
+        url: 'https://gelbooru.com/index.php?page=post&s=list&tags=%s',
+    },
+    yandere: {
+        url: 'https://yande.re/post?tags=%s',
+    },
+    sankaku: {
+        url: 'https://chan.sankakucomplex.com/?tags=%s',
+    },
+    konachan: {
+        url: 'https://konachan.com/post?tags=%s',
+    },
+};
 
 /***Functions***/
 
-function setCSSStyle(csstext) {
-    var css_dom = document.createElement('style');
-    css_dom.type = 'text/css';
-    css_dom.innerHTML = csstext;
-    document.head.appendChild(css_dom);
-}
+//Helper functions
 
-function RenderImageSiteLinks(encoded_tagname,num) {
-    return `
-    <div class="image-links" data-id="${num}">
-        <ul class="site-link-list">
-            <li class="site-link"><a href="http://www.pixiv.net/search.php?s_mode=s_tag_full&amp;word=${encoded_tagname}">Pixiv</a></li>
-            <li class="site-link"><a href="http://seiga.nicovideo.jp/tag/${encoded_tagname}">Nicoseiga</a></li>
-            <li class="site-link"><a href="http://nijie.info/search.php?word=${encoded_tagname}">Nijie</a></li>
-            <li class="site-link"><a href="http://www.tinami.com/search/list?keyword=${encoded_tagname}">Tinami</a></li>
-            <li class="site-link"><a href="http://bcy.net/tags/name/${encoded_tagname}">BCY.net</a></li>
-            <li class="site-link"><a href="http://www.deviantart.com/tag/${encoded_tagname}">Deviantart</a></li>
-            <li class="site-link"><a href="http://www.artstation.com/search?q=${encoded_tagname}">Artstation</a></li>
-            <li class="site-link"><a href="http://www.tumblr.com/tagged/${encoded_tagname}">Tumblr</a></li>
-            <li class="site-link"><a href="http://twitter.com/hashtag/${encoded_tagname}">Twitter</a></li>
-            <li class="site-link"><a href="http://e-hentai.org/?f_search=${encoded_tagname}">E-Hentai</a></li>
-        </ul>
-    </div>`;
-}
-
-function RenderBooruLinks(tagname,num) {
-    return `
-    <div class="booru-links" data-id="${num}">
-        <ul class="booru-link-list">
-            <li class="booru-link"><a href="http://gelbooru.com/index.php?page=post&s=list&tags=${tagname.replace(/ /g,'_')}">Gelbooru</a></li>
-            <li class="booru-link"><a href="http://yande.re/post?tags=${tagname.replace(/ /g,'_')}">Yandere</a></li>
-            <li class="booru-link"><a href="http://chan.sankakucomplex.com/?tags=${tagname.replace(/ /g,'_')}">Sankaku</a></li>
-            <li class="booru-link"><a href="http://konachan.com/post?tags=${tagname.replace(/ /g,'_')}">Konachan</a></li>
-        </ul>
-    </div>`;
-}
-
-function RenderTranslatedTags(tagname,encoded_tagname,num) {
-    return `
-<div class="wiki-other-name">
-    <span class="other-name-tagtext">${tagname}</span>
-    <a class="ui-icon collapsible-image-links ui-icon-triangle-1-e" data-id="${num}"></a>` +
-    RenderImageSiteLinks(encoded_tagname,num) + `
-</div>`;
-}
-
-function RenderMainTag(tagname) {
-    return `
-<div class="wiki-other-name">
-    <a class="ui-icon collapsible-booru-links ui-icon-triangle-1-w" data-id="a"></a>
-    <span class="other-name-tagtext">${tagname}</span>
-    <a class="ui-icon collapsible-image-links ui-icon-triangle-1-e" data-id="0"></a>` +
-    RenderImageSiteLinks(encodeURIComponent(tagname),0) +
-    RenderBooruLinks(tagname,'a') + `
-</div>`;
-}
-
-function isWikiPage() {
-    return ($("#wiki-page-title").length) || (($("#show-excerpt-link").html() === "Wiki") && ($("#excerpt .prose").length > 0));
+function IsWikiPage() {
+    return (STS.controller === 'wiki-pages') ||
+        ((STS.controller === 'posts') &&
+         ($('#show-excerpt-link').html() === "Wiki") &&
+         ($('#excerpt .prose').length > 0));
 }
 
 function GetWikiName() {
-    if (isWikiPage()) {
-        if ($("#c-posts #a-index").length) {
-            let tagname = $('title').html().trim();
-            return tagname.slice(0,tagname.lastIndexOf('-')-1);
-        } else if ($("#c-wiki-pages #a-show")) {
-            return $("#wiki-page-title a").html();
+    if (STS.controller === 'posts') {
+        let wiki_name = "";
+        let url = $('#show-excerpt-link').attr('href');
+        let match = url.match(/^\/wiki_pages\/([^/]+)/);
+        if (match) {
+            wiki_name = match[1];
         }
+        return decodeURIComponent(wiki_name);
+    } else if (STS.controller === 'wiki-pages') {
+        return $('#wiki-page-title a').html();
     }
 }
 
-/***Program start***/
+function AnySite(type) {
+    return Boolean(STS.user_settings[type + '_sites_enabled'].length);
+}
 
-function main() {
-    if (!isWikiPage()) {
+//Render functions
+
+function RenderSiteLinks(type,tagname,searchtag,num) {
+    let site_list = STS.user_settings[type + '_sites_enabled'].map((site)=>{
+        let url = JSPLib.utility.sprintf(SITE_CONFIG[site].url, searchtag);
+        let display_name = JSPLib.utility.displayCase(site);
+        return `<li class="sts-${type}-link"><a target="_blank" href="${url}">${display_name}</a></li>`;
+    });
+    return `
+<div class="sts-links sts-${type}-links" data-id="${num}">
+    <ul class="sts-${type}-link-list">
+        ${site_list.join('')}
+    </ul>
+</div>`;
+}
+
+function RenderSiteToggle(type,num,direction) {
+    if (AnySite(type)) {
+        return `<a class="ui-icon sts-collapsible-${type}-links ui-icon-triangle-1-${direction}" data-id="${num}"></a>`;
+    }
+    return "";
+}
+
+function RenderTranslatedTags(tagname,num) {
+    let rendered_source_tags = RenderSiteLinks('source', tagname, encodeURIComponent(tagname), num);
+    let source_toggle = RenderSiteToggle('source', num, 'e');
+    return `
+<div class="wiki-other-name sts-other-tag">
+    <span class="sts-other-tagtext">${tagname}</span>
+    ${source_toggle}
+    ${rendered_source_tags}
+</div>`;
+}
+
+function RenderMainTag() {
+    let tagname = GetWikiName();
+    let rendered_source_tags = RenderSiteLinks('source', tagname, encodeURIComponent(tagname), 0);
+    let rendered_booru_tags = RenderSiteLinks('booru', tagname, tagname.replace(/ /g, '_'), 'a');
+    let source_toggle = RenderSiteToggle('source', 0, 'e');
+    let booru_toggle = RenderSiteToggle('booru', 'a', 'w');
+    return `
+<div class="wiki-other-name sts-main-tag">
+    ${booru_toggle}
+    <span class="sts-main-tagtext">${tagname}</span>
+    ${source_toggle}
+    ${rendered_booru_tags}
+    ${rendered_source_tags}
+</div>`;
+}
+
+//Event handlers
+
+function SiteLinkToggle(type,direction) {
+    return function (event) {
+        let id = $(event.target).data('id');
+        $(`.sts-collapsible-${type}-links[data-id=${id}]`).toggleClass(`ui-icon-triangle-1-${direction} ui-icon-triangle-1-s`);
+        $(`.sts-${type}-links[data-id=${id}]`).slideToggle(100);
+    }
+}
+
+//Settings functions
+
+function RenderSettingsMenu() {
+    $('#site-tag-searches').append(STS_MENU);
+    $('#sts-general-settings').append(JSPLib.menu.renderDomainSelectors());
+    $('#sts-source-settings').append(JSPLib.menu.renderInputSelectors('booru_sites_enabled', 'checkbox'));
+    $('#sts-source-settings').append(JSPLib.menu.renderInputSelectors('source_sites_enabled', 'checkbox'));
+    JSPLib.menu.engageUI(true);
+    JSPLib.menu.saveUserSettingsClick();
+    JSPLib.menu.resetUserSettingsClick();
+}
+
+//Main function
+
+function Main() {
+    Danbooru.STS = STS = {
+        controller: document.body.dataset.controller,
+        action: document.body.dataset.action,
+        settings_config: SETTINGS_CONFIG,
+    };
+    Object.assign(STS, {
+        user_settings: JSPLib.menu.loadUserSettings(),
+    });
+    if (JSPLib.danbooru.isSettingMenu()) {
+        JSPLib.menu.loadStorageKeys();
+        JSPLib.utility.installScript(JQUERY_TAB_WIDGET_URL).done(()=>{
+            JSPLib.menu.installSettingsMenu();
+            RenderSettingsMenu();
+        });
+    }
+    if (!JSPLib.menu.isScriptEnabled()) {
+        Main.debuglog("Script is disabled on", window.location.hostname);
         return;
     }
-    setCSSStyle(program_css);
-
-    $(".wiki-other-name").each((i,entry)=>{
-        let tagname = entry.innerHTML;
-        let elem = document.createElement('textarea');
-        elem.innerHTML = tagname;
-        let decoded = elem.value;
-        let encoded_tagname = encodeURIComponent(decoded);
-        entry.outerHTML = RenderTranslatedTags(tagname,encoded_tagname,i+1);
-    });
-
-    if ($(".wiki-other-name").length) {
-        $(".wiki-other-name").parent().prepend(RenderMainTag(GetWikiName()));
-    } else {
-        let $elem = $('<p></p>').prepend(RenderMainTag(GetWikiName()));
-        $("#wiki-page-body").prepend($elem);
+    if (!IsWikiPage()) {
+        Main.debuglog("No wiki page bodies!");
+        return;
     }
-
-    $(".collapsible-image-links").click((e)=>{
-        let id = $(e.target).data('id');
-        $(`.collapsible-image-links[data-id=${id}]`).toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s");
-        $(`.image-links[data-id=${id}]`).slideToggle(100);
-        e.preventDefault();
-    });
-
-    $(".collapsible-booru-links").click((e)=>{
-        let id = $(e.target).data('id');
-        $(`.collapsible-booru-links[data-id=${id}]`).toggleClass("ui-icon-triangle-1-w ui-icon-triangle-1-s");
-        $(`.booru-links[data-id=${id}]`).slideToggle(100);
-        e.preventDefault();
-    });
+    let $wiki_other_names = $('.wiki-other-name');
+    if ($wiki_other_names.length) {
+        let $wiki_container = $wiki_other_names.parent();
+        $wiki_other_names.each((i,entry)=>{
+            entry.outerHTML = RenderTranslatedTags(entry.innerText, i + 1);
+        });
+        $wiki_container.prepend(RenderMainTag());
+    } else {
+        let $elem = $('<p></p>').prepend(RenderMainTag());
+        $('#wiki-page-body, #c-posts #a-index .prose').prepend($elem);
+    }
+    $('.sts-collapsible-source-links').on(PROGRAM_CLICK, SiteLinkToggle('source', 'e'));
+    $('.sts-collapsible-booru-links').on(PROGRAM_CLICK, SiteLinkToggle('booru', 'w'));
+    JSPLib.utility.setCSSStyle(PROGRAM_CSS);
 }
 
-if ($("#c-wiki-pages #a-show,#c-posts #a-index").length) {
-    main();
+/****Function decoration****/
+
+JSPLib.debug.addFunctionLogs([
+    Main
+]);
+
+/****Initialization****/
+
+//Variables for debug.js
+JSPLib.debug.debug_console = true;
+JSPLib.debug.pretext = 'STS:';
+JSPLib.debug.level = JSPLib.debug.INFO;
+
+//Variables for menu.js
+JSPLib.menu.program_shortcut = PROGRAM_SHORTCUT;
+JSPLib.menu.program_name = PROGRAM_NAME;
+
+//Export JSPLib
+if (JSPLib.debug.debug_console) {
+    window.JSPLib.lib = window.JSPLib.lib || {};
+    window.JSPLib.lib[PROGRAM_NAME] = JSPLib;
 }
+
+/****Execution start****/
+
+JSPLib.load.programInitialize(Main, 'STS', PROGRAM_LOAD_REQUIRED_VARIABLES, [], PROGRAM_LOAD_OPTIONAL_SELECTORS);
