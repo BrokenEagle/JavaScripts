7104ebc1fe3e66c19918fe5d30a5d54118ce3552
BrokenEagle <BrokenEagle98@yahoo.com>
2020-12-20 22:20:54 -0800

    IndexedAutocomplete: Fix render of multi-tags

--- indexedautocomplete.user.js
+++ indexedautocomplete.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IndexedAutocomplete
 // @namespace    https://github.com/BrokenEagle/JavaScripts
-// @version      28.6
+// @version      28.7
 // @description  Uses Indexed DB for autocomplete, plus caching of other data.
 // @source       https://danbooru.donmai.us/users/23799
 // @author       BrokenEagle
@@ -2305,7 +2305,7 @@ function DanbooruIntializeTagAutocomplete() {
     });
     $fields_multiple.each((i,entry)=>{
         let autocomplete = $(entry).data('uiAutocomplete');
-        autocomplete._render = Danbooru.Autocomplete._render;
+        autocomplete._renderItem = Danbooru.Autocomplete.render_item;
     });
 }
 
