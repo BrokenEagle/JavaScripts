BrokenEagle/JavaScripts

commit 8a8f4d79aeab9f0ebc69287338b94b991b0b7230

RecentTagsCalc: Don't activate on the non upload show pages

recenttagscalc.user.js
@@ -5,8 +5,8 @@
 // @description  Use different mechanism to calculate RecentTags.
 // @source       https://danbooru.donmai.us/users/23799
 // @author       BrokenEagle
-// @match        *://*.donmai.us/uploads/*
-// @match        *://*.donmai.us/posts/*
+// @include      /^https?://\w+\.donmai\.us/uploads/\d+(\?|$)/
+// @include      /^https?://\w+\.donmai\.us/posts/\d+(\?|$)/
 // @match        *://*.donmai.us/settings
 // @exclude      /^https?://\w+\.donmai\.us/.*\.(xml|json|atom)(\?|$)/
 // @grant        none
@@ -42,7 +42,7 @@ const DANBOORU_TOPIC_ID = '15851';
 
 //Variables for load.js
 const program_load_required_variables = ['window.jQuery','window.Danbooru','Danbooru.CurrentUser'];
-const program_load_required_selectors = ['#page'];
+const program_load_required_selectors = ['#c-uploads #a-show, #c-posts #a-show'];
 
 //Program name constants
 const PROGRAM_SHORTCUT = 'rtc';

commit 3b8081a59f5b9726b1284faaf70707923c56a917

RecentTagsCalc: Fix the script not showing up on the settings page
It was a regression caused by 8a8f4d79aeab9f0ebc69287338b94b991b0b7230.

recenttagscalc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RecentTagsCalc
 // @namespace    https://github.com/BrokenEagle/JavaScripts
-// @version      7.17
+// @version      7.18
 // @description  Use different mechanism to calculate RecentTags.
 // @source       https://danbooru.donmai.us/users/23799
 // @author       BrokenEagle
@@ -43,7 +43,7 @@ const DANBOORU_TOPIC_ID = '15851';
 
 //Variables for load.js
 const program_load_required_variables = ['window.jQuery','window.Danbooru','Danbooru.CurrentUser'];
-const program_load_required_selectors = ['#c-uploads #a-show, #c-posts #a-show'];
+const program_load_required_selectors = ['#c-uploads #a-show, #c-upload-media-assets #a-show, #c-posts #a-show, #c-users #a-edit'];
 
 //Program name constants
 const PROGRAM_SHORTCUT = 'rtc';