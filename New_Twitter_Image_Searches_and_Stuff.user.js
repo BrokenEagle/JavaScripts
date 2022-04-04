BrokenEagle/JavaScripts

commit 72de571c3e7f8c886ec7e9e23ce46386b8df080e

New TISAS: Fix API data not being scraped
The issue was that Twitter changed their API format... again. It was
causing users to not get the video links, as well as the user ID, etc.

New_Twitter_Image_Searches_and_Stuff.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New Twitter Image Searches and Stuff
 // @namespace    https://github.com/BrokenEagle/JavaScripts
-// @version      7.16
+// @version      7.17
 // @description  Searches Danbooru database for tweet IDs, adds image search links, and highlights images based on Tweet favorites.
 // @source       https://danbooru.donmai.us/users/23799
 // @author       BrokenEagle
@@ -4528,10 +4528,12 @@ function ProcessTwitterData(data) {
     }
 }
 
-function CheckGraphqlData(data,savedata=[]) {
+function CheckGraphqlData(data, savedata=[]) {
     for (let i in data) {
-        if ((i === "tweet" || i === "user") && 'legacy' in data[i] && 'rest_id' in data[i]) {
+        if ((i === "tweet" || i === "user") && ('legacy' in data[i]) && ('rest_id' in data[i])) {
             savedata.push({type: i, id: data[i].rest_id, item: data[i].legacy});
+        } else if ((i === 'result') && (data[i]?.__typename === 'Tweet' || data[i]?.__typename === 'User')) {
+            savedata.push({type: data[i].__typename.toLowerCase(), id: data[i].rest_id, item: data[i].legacy});
         }
         if (typeof data[i] === "object" && data[i] !== null) {
             CheckGraphqlData(data[i], savedata);

commit a541a96b73c95e87ff9233122c748c8ff5bc9a15

New TISAS: Fix exception when scraping Twitter API data
Reversion caused by 72de571c3e7f8c886ec7e9e23ce46386b8df080e, as the
presence of the rest_id and legacy subkeys was not being checked.

New_Twitter_Image_Searches_and_Stuff.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New Twitter Image Searches and Stuff
 // @namespace    https://github.com/BrokenEagle/JavaScripts
-// @version      7.17
+// @version      7.18
 // @description  Searches Danbooru database for tweet IDs, adds image search links, and highlights images based on Tweet favorites.
 // @source       https://danbooru.donmai.us/users/23799
 // @author       BrokenEagle
@@ -4530,12 +4530,14 @@ function ProcessTwitterData(data) {
 
 function CheckGraphqlData(data, savedata=[]) {
     for (let i in data) {
-        if ((i === "tweet" || i === "user") && ('legacy' in data[i]) && ('rest_id' in data[i])) {
-            savedata.push({type: i, id: data[i].rest_id, item: data[i].legacy});
-        } else if ((i === 'result') && (data[i]?.__typename === 'Tweet' || data[i]?.__typename === 'User')) {
-            savedata.push({type: data[i].__typename.toLowerCase(), id: data[i].rest_id, item: data[i].legacy});
-        }
         if (typeof data[i] === "object" && data[i] !== null) {
+            if (('legacy' in data[i]) && ('rest_id' in data[i])) {
+                if ((i === "tweet" || i === "user")) {
+                    savedata.push({type: i, id: data[i].rest_id, item: data[i].legacy});
+                } else if ((i === 'result') && (data[i]?.__typename === 'Tweet' || data[i]?.__typename === 'User')) {
+                    savedata.push({type: data[i].__typename.toLowerCase(), id: data[i].rest_id, item: data[i].legacy});
+                }
+            }
             CheckGraphqlData(data[i], savedata);
         }
     }