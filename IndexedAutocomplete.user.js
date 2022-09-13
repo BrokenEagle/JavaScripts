6310ce6826343d8651fed4145c4e898e726e3e61
BrokenEagle <BrokenEagle98@yahoo.com>
2022-09-12 23:41:53 -0700

    IndexedAutocomplete: Fix script clobbering regular usage of autocomplete

--- IndexedAutocomplete.user.js
+++ IndexedAutocomplete.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IndexedAutocomplete
 // @namespace    https://github.com/BrokenEagle/JavaScripts
-// @version      29.8
+// @version      29.9
 // @description  Uses Indexed DB for autocomplete, plus caching of other data.
 // @source       https://danbooru.donmai.us/users/23799
 // @author       BrokenEagle
@@ -1554,6 +1554,9 @@ function ExpirationTime(type, count) {
 //Render functions
 
 function AutocompleteRenderItem(list, item) {
+    if ('html' in item) {
+        return Danbooru.Autocomplete.render_item_old(list, item);
+    }
     var $link = $("<a/>");
     $link.text(item.label);
     $link.attr("href", "/posts?tags=" + encodeURIComponent(item.value));
