5f31fcf4b1462de195f41e451f9f96e803688758
BrokenEagle <BrokenEagle98@yahoo.com>
2023-01-21 10:48:51 -0800

    IndexedAutocomplete: Bump version

--- IndexedAutocomplete.user.js
+++ IndexedAutocomplete.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IndexedAutocomplete
 // @namespace    https://github.com/BrokenEagle/JavaScripts
-// @version      29.17
+// @version      29.18
 // @description  Uses Indexed DB for autocomplete, plus caching of other data.
 // @source       https://danbooru.donmai.us/users/23799
 // @author       BrokenEagle
