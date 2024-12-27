b2bf79e858ab559cd558154f2a052bef1695d45e
BrokenEagle <BrokenEagle98@yahoo.com>
2024-12-27 12:38:30 -0800

    IndexedAutocomplete: Bump minor version
    This was missed in the last commit which updated the library.

--- IndexedAutocomplete.user.js
+++ IndexedAutocomplete.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IndexedAutocomplete
 // @namespace    https://github.com/BrokenEagle/JavaScripts
-// @version      29.21
+// @version      29.22
 // @description  Uses Indexed DB for autocomplete, plus caching of other data.
 // @source       https://danbooru.donmai.us/users/23799
 // @author       BrokenEagle
