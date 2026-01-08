BrokenEagle/JavaScripts

commit e595cfa15b0e0752158b407a5b061a9e6ca34435

IndexedAutocomplete: Use the input for grabbing the completion
Since the completion no longer contains the prefix or operator.

IndexedAutocomplete.user.js
@@ -1768,18 +1768,16 @@ function InsertCompletion(input, completion) {
         before_caret_text = before_caret_text.replace(/^[ \t]+|[ \t]+$/gm, "");
         after_caret_text = after_caret_text.replace(/^[ \t]+|[ \t]+$/gm, "");
         var query = ParseQuery(input.value, input.selectionStart);
-        var select = ParseQuery(completion, completion.length);
         before_caret_text = before_caret_text.substring(0, before_caret_text.search(/\S+$/));
-        var prefix = (query.metatag !== "tag" ? (query.prefix || select.prefix) : query.operator);
-        var name = (query.metatag === "tag" ? completion : select.term);
-        before_caret_text += prefix + name + ' ';
+        var prefix = (input.id === 'post_tag_string' ? query.prefix : query.operator);
+        before_caret_text += prefix + completion + ' ';
         start = end = before_caret_text.length;
     }
     input.value = before_caret_text + after_caret_text;
     input.selectionStart = start;
     input.selectionEnd = end;
     $(input).trigger("input");
-    $(() => $(input).autocomplete("instance").close());
+    setTimeout(() => {$(input).autocomplete("instance").close();}, JQUERY_DELAY);
 }
 
 function StaticMetatagSource(term, metatag) {

commit 45c0f9d54b5bce2d1d99d7a5d7362599c3ce079a

IndexedAutocomplete: Fix autocomplete box randomly appearing to close
This was happening if typing immediately after submitting a completion.
Caused by e595cfa. The original thought was that the jQuery wrapping
the close function was executed immediately, so to make things clearer
a setTimeout was used instead, however the delay was not short enough,
and a half a second was long enough for the box to close after typing
again.

IndexedAutocomplete.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IndexedAutocomplete
 // @namespace    https://github.com/BrokenEagle/JavaScripts
-// @version      29.30
+// @version      29.31
 // @description  Uses Indexed DB for autocomplete, plus caching of other data.
 // @source       https://danbooru.donmai.us/users/23799
 // @author       BrokenEagle
@@ -1823,7 +1823,7 @@ function InsertCompletion(input, completion) {
     input.selectionStart = start;
     input.selectionEnd = end;
     $(input).trigger("input");
-    setTimeout(() => {$(input).autocomplete("instance").close();}, JQUERY_DELAY);
+    setTimeout(() => {$(input).autocomplete("instance").close();}, 1);
 }
 
 function StaticMetatagSource(term, metatag) {